95513c2bcdb6b14ebf8832d2c27bfca08e647d2c
janniemathilde <117343519+janniemathilde@users.noreply.github.com>
2023-03-10 10:12:38 +0100

    Add files via upload

---
+++ Silverline/app.js
@@ -0,0 +1,13 @@
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+    expandImg.parentElement.style.padding = "20px 10px 20px 35%";
+    //   expandImg.style.width = "30%";
+  }
+  
